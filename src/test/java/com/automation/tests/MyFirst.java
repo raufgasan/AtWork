2f9acbb5d3a79c40e4c1457a4abe5647c26766f9
raufgasan <raufgasan@gmail.com>
2020-03-03 10:37:34 -0800

    first frm work

---
+++ src/test/java/com/automation/tests/MyFirst.java
@@ -0,0 +1,15 @@
+package com.automation.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirst {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+    }
+}
